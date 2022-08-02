9d82af962e072170997ef890942558c203d25b8c
root <root@SANADA-Lenovo-TP-X1C.localdomain>
2022-08-02 18:42:47 +0900

    comp CPU

--- TODAI_CPU/TODAI_CPU.srcs/sources_1/new/fetch.v
+++ TODAI_CPU/TODAI_CPU.srcs/sources_1/new/fetch.v
@@ -1,6 +1,6 @@
 module fetch (pc, ins) ; 
     input [31:0] pc; 
     output [31:0] ins; 
-    reg [31:0] ins_mem [0:255] ; 
-    assign ins = ins_mem [pc] ; 
+    reg [31:0] ins_mem[0:255] ; 
+    assign ins = ins_mem[pc] ; 
 endmodule
