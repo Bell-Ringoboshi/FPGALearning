943d7169f0bd672c53ade4a36b32520b097d324a
Bell-Ringoboshi <sanarin1208@outlook.jp>
2022-08-08 15:46:33 +0900

    grad comp

--- Display_view/Display_view.srcs/sources_1/new/pattern.v
+++ Display_view/Display_view.srcs/sources_1/new/pattern.v
@@ -1,5 +1,3 @@
-
-
 module pattern(
     input CLK,
     input RST,
@@ -11,30 +9,44 @@ module pattern(
 
     `include "vga_param.vh"
 
-    localparam HSIZE = 64;
-    localparam VSIZE = 120;
+    localparam HSIZE = 320;
+    localparam VSIZE = 60;
 
     wire[9:0] HCNT,VCNT;
-
     syncgen syncgen(
         .CLK(CLK),
         .RST(RST),
         .PCK(PCK),
         .VGA_HS(VGA_HS),
-        .VGA_VS(VGA_VS),
-        
+        .VGA_VS(VGA_VS),         
         .HCNT(HCNT),
-        .VCNT(VCNT)
+        .VCNT(VCNT) 
     );
 
     wire[9:0] HBLANK = HFRONT + HWIDTH + HBACK;
     wire[9:0] VBLANK = VFRONT + VWIDTH + VBACK;
-    wire[9:0] CCNT = HCNT - HBLANK + 1;
-    wire[3:0] INDCNT = CCNT[5:2];
+    wire[9:0] CCNT   = HCNT - HBLANK + 1;
+    wire[7:0] INDCNT = (CCNT[9:0] * 255) / 640;
     
     wire disp_enable = (VBLANK <= VCNT) && (HBLANK - 10'd1 <= HCNT) && (HCNT < HPERIOD - 10'd1);
 
-    wire[2:0] rgb_0 = 7 - ((VCNT - VBLANK + 1) / VSIZE) - ((2 * (((VCNT - VBLANK + 1) / VSIZE) >=1)) + (((VCNT - VBLANK + 1) / VSIZE) >=2));
+    reg[2:0] rgb_0 ;
+
+    always @* begin
+        if(/*(CCNT / HSIZE) <*/ 1)begin
+            case ((VCNT - VBLANK + 1) / VSIZE)
+                0: rgb_0 = 7;
+                1: rgb_0 = 7;
+                2: rgb_0 = 7;
+                3: rgb_0 = 7;
+                default: rgb_0 = 7;
+            endcase
+        end
+        else begin
+            rgb_0 <= 1;
+        end
+    end
+
     wire[2:0] rgb_1 = rgb_0;
 
     always @(posedge PCK)begin
@@ -42,9 +54,9 @@ module pattern(
             {VGA_R,VGA_G,VGA_B} <= 0;
         end
         else if(disp_enable) begin
-            VGA_R <= rgb_1[2] ? {INDCNT,INDCNT}:0; 
-            VGA_G <= rgb_1[1] ? {INDCNT,INDCNT}:0; 
-            VGA_B <= rgb_1[0] ? {INDCNT,INDCNT}:0; 
+            VGA_R = INDCNT * rgb_1[2];
+            VGA_G = INDCNT * rgb_1[1];
+            VGA_B = INDCNT * rgb_1[0];
         end
         else {VGA_R,VGA_G,VGA_B} <= 0;
     end
@@ -55,4 +67,3 @@ module pattern(
     end
 
 endmodule
-
